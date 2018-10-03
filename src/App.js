afb83e1e34c7a364ebf3f0747a93e777e1690a14
americanman <snowboardracer7@gmail.com>
2018-10-03 15:10:48 -0400

    Add Card

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Card = (props) => {
+	return(
+  	<div style={{margin: '1em', display: 'flex'}}>
+			<img src="http://placehold.it/75"/>
+      <div className='info' style={{margin: '1em'}}>
+      	<div>Name here ..</div>
+        <div>Company name</div>
+      </div>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Card />
     );
   }
 }
