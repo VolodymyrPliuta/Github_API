560f56a6d2fef05e6c0f6ea16a68561979695fe7
americanman <snowboardracer7@gmail.com>
2018-10-03 15:53:27 -0400

    Map props

--- src/App.js
+++ src/App.js
@@ -3,28 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 const Card = (props) => {
-	return(
-  	<div style={{margin: '1em', display: 'flex'}}>
-			<img src="http://placehold.it/75"/>
+  return(
+    <div style={{margin: '1em', display: 'flex'}}>
+      <img style={{height: '10em'}} src={props.avatarURL}/>
       <div className='info' style={{margin: '1em'}}>
-      	<div>Name here ..</div>
+        <div style={{fontSize: '2.25em', fontWeight: 'bold'}}>Name here ..</div>
         <div>Company name</div>
       </div>
     </div>
   )
 }
 
+let data = [
+  {
+    name: "John",
+    avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
+    company: "Facebook"
+  },
+  {
+    name: "Nick",
+    avatar_url: "https://avatars0.githubusercontent.com/u/5?v=4",
+    company: "Google"
+  }
+]
+
 const CardList = (props) => {
-	return(
-  	<Card />
+  return(
+    <div>
+      {props.cards.map(card => <Card avatarURL={card.avatar_url} />)}
+    </div>
   )
 }
 
-ReactDOM.render(<CardList />, mountNode);
 class App extends Component {
   render() {
     return (
-      <Card />
+      <CardList cards={data}/>
     );
   }
 }
